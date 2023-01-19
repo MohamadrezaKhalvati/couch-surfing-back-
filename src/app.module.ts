b07976f03246d4eeaa0c1347d7c632e1bde12169
mohamadreza <mohamadrezakhf@Gmail.com>
2023-01-19 18:09:16 +0330

    fix schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from './module/auth/auth.module'
+import { EventModule } from './module/event/event.module'
+import { HostModule } from './module/host/host.module'
 import { PrismaModule } from './module/prisma/prisma.module'
+import { TravelModule } from './module/travel/travel.module'
 @Module({
-	imports: [AuthModule, PrismaModule],
+	imports: [AuthModule, PrismaModule, EventModule, TravelModule, HostModule],
 })
 export class AppModule {}
